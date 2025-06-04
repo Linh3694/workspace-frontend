efc050aad30f64e496db0db9406ee61bd42b33ec
Linh3694 <88594329+Linh3694@users.noreply.github.com>
2025-06-04 21:53:15 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/workspace-frontend/' : '/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
